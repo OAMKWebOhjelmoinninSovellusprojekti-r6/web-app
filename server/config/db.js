6f4ea616eb2d5bc4d1e333a16653d662f856b512
Henry Väisänen <t5vahe01@students.oamk.fi>
2022-03-17 19:38:45 +0200

    closes #7 Create User model
    - Created User model
    - Added argon2 hashing algorithm to handle sensitive user data
    - Added necessary configuration for database operations
    	- Added necessary configuration for environment variables
    
    New npm dependencies:
    - argon2
    - dotenv
    - mysql

---
+++ server/config/db.js
@@ -0,0 +1,30 @@
+require('dotenv').config(); // TODO This should be moved on top of index.js in future, no need to have this multiple times in application
+const mysql = require('mysql');
+
+// Define database connection parameters
+const pool = mysql.createPool({
+  connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,    
+  password: process.env.MYSQL_PASSWORD,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+}); 
+
+// Every database query in application is created with this function
+// Function takes raw sql query string as parameter. eg. 'SELECT * FROM `user`' and handles request/response with Promise
+async function query(sql){
+  return new Promise((resolve, reject)=>{
+    pool.query(sql,  (error, elements)=>{
+      // Release connection from database so it doesnt stay open for nothing
+      pool.end();
+        if(error){
+            return reject(error);
+        }
+        return resolve(elements);
+    });
+  });
+}
+
+module.exports = {
+  query
+}
