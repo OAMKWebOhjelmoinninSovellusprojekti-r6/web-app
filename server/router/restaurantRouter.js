28ad2ac9c472398768a952e98c08ac9402f4a1f7
Gerhard Klemola <gerhardcarlklemola@protonmail.com>
2022-03-24 10:53:38 +0200

    close #41
    Implemented try/catch and value type check to POST and DELETE methods

--- server/router/restaurantRouter.js
+++ server/router/restaurantRouter.js
@@ -14,7 +14,7 @@ restaurantData = {
 
 // GET method route
 router.get('/:restaurantId?', (req, res) => {
-    // Function to send the request to the restaurant GET model executed when called
+    // Function to send the request to the restaurant 'get' model executed when called
     const getRestaurants = async () => {
         const data = await restaurantModel.get(req.params.restaurantId);
         console.log("response", data);
@@ -61,7 +61,7 @@ router.post('/', (req, res) => {
         user_iduser: 2
       }
 */   
-    // Function to send the request to the restaurant POST model executed when called
+    // Function to send the request to the restaurant 'create' model executed when called
     const addRestaurant = async () => {
         const data = await restaurantModel.create(req.body);
         console.log("created", data);
@@ -69,6 +69,7 @@ router.post('/', (req, res) => {
     };
     console.log(typeof req.body.restaurant_type);
     try {
+        // Check for correct type values, if incorrect throw to error handler
         console.log(typeof req.body.user_iduser, req.body.user_iduser);
         if(typeof req.body.name != 'string' 
             || typeof req.body.address != 'string' 
@@ -76,11 +77,13 @@ router.post('/', (req, res) => {
             || typeof req.body.image_path != 'string'
             || typeof req.body.restaurant_type != 'number'
             || typeof req.body.price_level != 'number'
-            || typeof req.body.user_iduser != 'number'
-           ) {
+            || typeof req.body.user_iduser != 'number') 
+        {
             throw new Error();
         };
+        // Calls the addRestaurant function, r represents the response data from the database
         addRestaurant().then(r => {
+            // If query was succesful send the received data, otherwise send the provided status code
             if (r.status === 200) {
                 console.log(r);
                 res.sendStatus(r.status);
@@ -89,6 +92,7 @@ router.post('/', (req, res) => {
             }
         });
     } catch (err) {
+        // Error response for incorrect valuetypes
         console.log("errorhandler executed");
         res.send("Incorrect value types")
     }
@@ -97,23 +101,85 @@ router.post('/', (req, res) => {
 // PUT method route
 router.put('/:restaurantId', (req, res) => {
 
+/** Example JSON data request, modify method does not need all the values neccessarily
+    {
+        name: 'the Mesta',
+        address: 'alley 7',
+        opening_hours: '9.00-23.00',
+        image_path: 'https://imagelocation.jpg',
+        restaurant_type: 3,
+        price_level: 2,
+        user_iduser: 2
+      }
+*/   
+    // Function to send the request to the restaurant 'modify' model executed when called
     const modifyRestaurant = async () => {
         let data = await restaurantModel.modify(req.params.restaurantId, req.body);
         console.log("Modified", data);
+        return data;
     }
-    modifyRestaurant();
-    res.send("modified");
+    console.log("typeof", typeof req.body.address);
+    try {
+        if((typeof req.body.name !== 'string' && req.body.name != undefined)
+            ||(typeof req.body.address != 'string' && req.body.address != undefined)
+            ||(typeof req.body.opening_hours != 'string' && req.body.opening_hours != undefined)
+            ||(typeof req.body.image_path != 'string' && req.body.image_path != undefined)
+            ||(typeof req.body.restaurant_type != 'number' && req.body.restaurant_type != undefined)
+            ||(typeof req.body.price_level != 'number' && req.body.price_level != undefined)
+            ||(typeof req.body.user_iduser != 'number' && req.body.user_iduser != undefined)
+
+        ) {
+            throw new Error();
+        }
+        // Calls the modifyRestaurant function, r represents the response data from the database
+        modifyRestaurant().then(r => {
+            // If query was succesful send the received data, otherwise send the provided status code
+            if (r.status === 200) {
+                console.log(r);
+                res.sendStatus(r.status);
+            } else {
+                res.sendStatus(r.status)
+            }
+        });
+    } catch (err) {
+        // Error response for incorrect valuetypes
+        console.log("errorhandler executed");
+        res.send("Incorrect value types")
+    }
 });
 
 // DELETE method route
-router.delete('/:restaurantId', (req, res) => {
+router.delete('/:restaurantId?', (req, res) => {
 
-    const delData = async () => {
+    // Function to send the request to the restaurant 'delete' model executed when called
+    const deleteRestaurant = async () => {
         let data = await restaurantModel.delete(req.params.restaurantId);
         console.log("deleted", data);
+        return data;
     }
-    delData();
-    res.send("deleted");
+    console.log(typeof req.params.restaurantId);
+    try {
+        // Transform the value to Integer
+        req.params.restaurantId = parseInt(req.params.restaurantId);
+        console.log(typeof req.params.restaurantId, req.params.restaurantId);
+        // If not correct value type throw to the error clause
+        if(req.params.restaurantId === undefined || isNaN(req.params.restaurantId)) {
+            throw new Error();
+        }
+        // Calls the deleteRestaurant function, r represents the responsedata from the database
+        deleteRestaurant().then(r => {
+            // If query was succesful send the received data, otherwise send the provided status code
+            if (r.status === 200) {
+                res.sendStatus(r.status);
+            } else {
+                res.sendStatus(r.status);
+            }
+        });
+    } catch (err) {
+        // DELETE method only uses ID for parameter so if not provided send error message
+        console.log("errorhandler executed");
+        res.send("No ID defined");
+    }
 });
 
 
