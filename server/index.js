4fc2f0936ebfc5158ca1fe7858b5d3d94e717c51
Henry Väisänen <t5vahe01@students.oamk.fi>
2022-03-30 00:51:33 +0300

    close #40 Added User router

--- server/index.js
+++ server/index.js
@@ -4,13 +4,11 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 const restaurantRouter = require('./router/restaurantRouter')
+const userRouter = require('./router/userRouter.js');
 
 app.use(bodyParser.json());
 app.use('/restaurant', restaurantRouter);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/user', userRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
