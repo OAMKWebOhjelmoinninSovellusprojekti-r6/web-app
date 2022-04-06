8c284cf73de61483e93f5d2fca60a8eb5b71f0f1
Henry Väisänen <t5vahe01@students.oamk.fi>
2022-04-06 16:31:33 +0300

    NAVBAR NAME FIX

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useState, useEffect, Fragment }  from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Register from './components/Register/Register';
-import NavBar from './components/NavBar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import RestaurantListView from './components/Restaurant/RestaurantListView';
 import Cart from './components/ShoppingCart/ShoppingCartView'
 import MenuListView from './components/Menu/MenuListView'
@@ -18,7 +18,7 @@ function App() {
   
   return (
   <BrowserRouter>
-  <NavBar/>
+  <Navbar/>
       <Routes>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/cart" element={<Cart address={userAddress}/>} />
